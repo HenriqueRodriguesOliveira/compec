602c34626f2dcac5df626b1b20db854f02649311
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2023-11-11 20:45:37 -0300

    Design System com Restyle

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import {ThemeProvider} from '@shopify/restyle';
+import {Button} from './src/components/Button/Button';
+
 import {Text} from './src/components/Text/Text';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text italic bold>
-        CoffStack
-      </Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text color="gray1" preset="headingLarge" marginBottom="s16" italic>
+            Compec
+          </Text>
+
+          <Button title="Entrar" marginBottom="s12" preset="primary" />
+
+          <Button title="Cadastrar" preset="outline" marginBottom="s12" />
+          <Button loading title="Loading" marginBottom="s12" />
+          <Button title="Desabilitado" disabled marginBottom="s12" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
